e9799e5cc75b1e9d1b205a6aea13382e5883e56b
Dominik Kluske <dome.k28@yahoo.com>
2025-07-15 20:18:24 +0200

    automatically fill mailto link on input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+document.addEventListener('input', updateSendMailButton);
+
 function toggleMobileMenu() {
     document.getElementById('mobile-nav-bar').classList.toggle('hidden');
+}
+
+function updateSendMailButton() {
+    let topic = document.getElementById('contact-topic');
+    let message = document.getElementById('contact-description');
+
+    let sendMailButton = document.getElementById('send-email-link');
+
+    sendMailButton.href = 'mailto:dominik.kluske@proton.me?subject=' + topic.value + '&body=' + message.value
 }
